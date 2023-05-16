bee58d6f32c79c842ed0a6767fee6ff2b17e6826
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-16 12:18:13 +0300

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItem = ({preview, original, description}) => {
+    return `  
+     <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+
+const makeGalleryItemMarup = galleryItems
+    .map(createGalleryItem)
+    .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryItemMarup);
+
+
+    
+
+   
+  
+
+
+  
+
+
